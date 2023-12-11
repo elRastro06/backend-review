0f65feb0f455d4ba254076b2d7d2979582cde340
artachojf <pepe.artacho.martin@uma.es>
2023-12-11 10:45:30 +0100

    Docker compose

--- conn.mjs
+++ conn.mjs
@@ -3,7 +3,11 @@
 import { MongoClient } from "mongodb";
 import 'dotenv/config';                 //esta libreria se encarga de cargar las credencias del .env
 
-const connectionString = process.env.CONNECTION_STRING;
+const user = process.env.MONGO_USER;
+const pwd = process.env.MONGO_PWD;
+const field1 = process.env.MONGO_CONNECTION_FIELD_1;
+const field2 = process.env.MONGO_CONNECTION_FIELD_2;
+const connectionString = `mongodb+srv://${user}:${pwd}@${field1}.${field2}.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(connectionString);
 let conn;
 try {
