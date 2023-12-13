a2efe8e02015992ddc4d98820fa3cbb1fa237736
artachojf <pepe.artacho.martin@uma.es>
2023-12-13 13:10:00 +0100

    Token verification and access control

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
-//inicio
-
 import express from "express";
 import v2 from "./v2.mjs";
 import cors from "cors";
 import axios from "axios";
+import { ObjectId } from "mongodb";
+import reviews from "./conn.mjs";
 
 const app = express();
 const port = 5008;
@@ -21,6 +21,13 @@ const clients = process.env.CLIENTS != undefined ? process.env.CLIENTS : "localh
 const verifyToken = async (req, res, next) => {
   try {
     const response = await axios.get(`http://${clients}:5000/checkToken/${req.headers.authorization}`);
+    const user = response.data.user;
+
+    if ((req.method == 'PUT' || req.method == 'DELETE') && req.params.id != undefined) {
+        const review = await reviews.findOne({ _id: new ObjectId(req.params.id) });
+        if (review.reviewerID != user._id) res.status(402).send("Unauthorized action");
+    } else if (req.method == 'POST' && req.body.reviewerID != user._id) res.status(402).send("Unauthorized action");
+
     next();
   } catch {
     res.status(401).send({ error: "Invalid token" });
